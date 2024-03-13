5df66cab4ffd642be01c91f7bfdb9da29e0d5d34
Nayeem <nayeem2281998@gmail.com>
2024-03-13 15:53:53 +0530

    about us and contact us page completed

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import FirstSection from '../components/AboutUs/FirstSection'
+import SecondSecontion from '../components/AboutUs/SecondSecontion'
 
 function AboutUs() {
   return (
     <div>
-      AboutUs
+      <FirstSection/>
+      <SecondSecontion/>
     </div>
   )
 }
